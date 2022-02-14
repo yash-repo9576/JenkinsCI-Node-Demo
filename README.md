ac20bf8f6cd599aa6d04fe44ebb4cdd7783d6c53
yash-repo9576 <yashrajput9576@gmail.com>
2022-02-14 23:59:08 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a Express.js Sample project.
+
+## Getting Started
+
+First,run npm install and then run the server:
+
+```bash
+npm install
+
+node index.js
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({"response": "Hello World!! NodeJS/ExpressJS working..."})
+});
+
+app.get('/welcome', (req, res) => {
+    res.json({"response": "Welcome to Sample API from yash-repo9576"})
+});
+
+app.listen(3000, () => {
+    console.log("Server started at http://localhost:3000");
+});
+module.exports = app;
