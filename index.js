e9a7c286e5b262ca24f6c4e56d3fad8bff3d2749
yash-repo9576 <yashrajput9576@gmail.com>
2022-02-15 11:24:19 +0530

    Checking Jenkins Trigger

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/welcome', (req, res) => {
     res.json({"response": "Welcome to Sample API from yash-repo9576"})
 });
 
+app.get('/test', (req, res) => {
+    res.json({"response": "Testing Jernkins Trigger..."})
+});
+
 app.listen(3000, () => {
     console.log("Server started at http://localhost:3000");
 });
